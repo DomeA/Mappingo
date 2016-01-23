11d8573884d8e17a1ce3562d246311f821779c1e
DomeA <domeastudio@gmail.com>
2016-01-23 14:45:42 +0800

    New changelist

---
+++ Utility/src/main/java/com/domeastudio/mappingo/application/util/serialization/JacksonMapper.java
@@ -0,0 +1,103 @@
+package com.domeastudio.mappingo.application.util.serialization;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * 单例模式
+ * Created by domea on 15-2-2.
+ */
+public class JacksonMapper {
+    protected Log logger = LogFactory.getLog(getClass());
+    private ObjectMapper mapper;
+    /**
+     *
+     */
+    public JacksonMapper(Inclusion inclusion) throws Exception {
+        mapper = new ObjectMapper();
+        //设置输出包含的属性
+        mapper.getSerializationConfig().setSerializationInclusion(inclusion);
+        //设置输入时忽略JSON字符串中存在而Java对象实际没有的属性
+        mapper.getDeserializationConfig().set(
+                org.codehaus.jackson.map.DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+    /**
+     * 创建输出全部属性到Json字符串的Binder.
+     */
+    public static JacksonMapper buildNormalBinder() throws Exception{
+        return new JacksonMapper(Inclusion.ALWAYS);
+    }
+
+    /**
+     * 创建只输出非空属性到Json字符串的Binder.
+     */
+    public static JacksonMapper buildNonNullBinder() throws Exception{
+        return new JacksonMapper(Inclusion.NON_NULL);
+    }
+
+    /**
+     * 创建只输出初始值被改变的属性到Json字符串的Binder.
+     */
+    public static JacksonMapper buildNonDefaultBinder() throws Exception{
+        return new JacksonMapper(Inclusion.NON_DEFAULT);
+    }
+
+    /**
+     * 如果JSON字符串为Null或"null"字符串,返回Null.
+     * 如果JSON字符串为"[]",返回空集合.
+     *
+     * 如需读取集合如List/Map,且不是List<String>这种简单类型时使用如下语句:
+     * List<MyBean> beanList = binder.getMapper().readValue(listString, new TypeReference<List<MyBean>>() {});
+     */
+    public <T> T fromJson(String jsonString, Class<T> clazz){
+        if (StringUtils.isEmpty(jsonString)) {
+            return null;
+        }
+
+        try {
+            return mapper.readValue(jsonString, clazz);
+        } catch (IOException e) {
+            logger.warn("parse json string error:" + jsonString, e);
+            return null;
+        }
+    }
+
+    /**
+     * 如果对象为Null,返回"null".
+     * 如果集合为空集合,返回"[]".
+     */
+    public String toJson(Object object){
+
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (IOException e) {
+            logger.warn("write to json string error:" + object, e);
+            return null;
+        }
+    }
+
+    /**
+     * 设置转换日期类型的format pattern,如果不设置默认打印Timestamp毫秒数.
+     */
+    public void setDateFormat(String pattern)throws Exception {
+        if (StringUtils.isNotBlank(pattern)) {
+            DateFormat df = new SimpleDateFormat(pattern);
+            mapper.getSerializationConfig().setDateFormat(df);
+            mapper.getDeserializationConfig().setDateFormat(df);
+        }
+    }
+
+    /**
+     * 取出Mapper做进一步的设置或使用其他序列化API.
+     * @return
+     */
+    public ObjectMapper getInstance()throws Exception {
+        return mapper;
+    }
+}
