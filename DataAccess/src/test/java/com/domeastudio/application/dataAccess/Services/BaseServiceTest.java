8941922c4eda4d4df78f2eebe654b329e2e9e2ac
DomeA <domeastudio@gmail.com>
2016-01-31 22:59:32 +0800

    添加测试类

---
+++ DataAccess/src/test/java/com/domeastudio/application/dataAccess/Services/BaseServiceTest.java
@@ -0,0 +1,22 @@
+package com.domeastudio.application.dataAccess.Services;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by domea on 16-1-31.
+ */
+public class BaseServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
